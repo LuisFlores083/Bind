1dd6eb98cec2fe380e3afe56c80ae18ce790c9cd
Luis Flores <93547811+LuisFlores083@users.noreply.github.com>
2023-04-10 11:00:02 -0600

    Actualizacion

--- js/script.js
+++ js/script.js
@@ -3,6 +3,28 @@
 
     data: {
       colorFondo: '',
+      validacion:'Escribe un nombre mayor a 3 letras',
+      nombre:'',
+      tamanio:12,
+
         
+    }, 
+    methods:{
+      validar:function(){
+        if(this.nombre.length == 0){
+          this.validacion ='Escribe un nombre con un minimo de 4 letras';
+          this.clase=0;
+        }
+        else if( this.nombre.length > 0 && this.nombre.length < 4){
+          this.validacion ='Nombre Invalido';
+          this.clase=2;
+        }
+        else {
+          this.validacion ='Nombre Valido';
+          this.clase=1;
+        }
+          
+      },
+
     }
  });
