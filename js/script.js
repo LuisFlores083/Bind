02fd37fc9f1df3622b155966701d32f5bda1542b
Luis Flores <93547811+LuisFlores083@users.noreply.github.com>
2023-04-11 09:57:06 -0600

    Actualizacion 2

--- js/script.js
+++ js/script.js
@@ -6,6 +6,10 @@
       validacion:'Escribe un nombre mayor a 3 letras',
       nombre:'',
       tamanio:12,
+      cantidad:0,
+      num1:1,
+      num2:1,
+      resultado:0,
 
         
     }, 
@@ -26,5 +30,28 @@
           
       },
 
-    }
+      sumarNumeros(){
+        this.resultado=this.num1+this.num2;
+      }
+
+    },
+
+    computed:{
+      resultadoSuma(){
+        return this.num1+this.num2;
+      },
+      resultadoResta(){
+        return this.num1-this.num2;
+      },
+      resultadoMulti(){
+        return this.num1*this.num2;
+      },
+      resultadoDivi(){
+        return this.num1/this.num2;
+      },
+      resultadoModul(){
+        return this.num1%this.num2;
+      },
+    },
+    
  });
